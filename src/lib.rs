8c5681929048ce831bacde0362433d535576d096
Pedro Santayana <pedrosantayana@gmail.com>
2022-02-14 19:03:34 -0300

    a

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,43 @@
-#[allow(dead_code)]
 pub struct BitVec {
     pub vec: Vec<u8>,
-    length: usize
+    length: usize,
 }
 
-#[allow(dead_code)]
 impl BitVec {
     pub fn new() -> Self {
         BitVec {
-            vec: vec![0],
-            length: 0
+            vec: Vec::new(),
+            length: 0,
         }
     }
 
+    pub fn len(&self) -> usize {
+        self.length
+    }
+
     pub fn read(&mut self, index: usize) -> bool {
-        let cell = self.vec[index/8];
+        let cell = self.vec[index / 8];
         ((cell >> index % 8) & 1) == 1
     }
 
     pub fn write(&mut self, index: usize, value: bool) {
+        while self.vec.len() < index / 8 + 1 {
+            self.vec.push(0);
+        }
         self.vec[index / 8] |= (value as u8) << (index % 8);
+    }
+
+    pub fn push(&mut self, value: bool) {
+        self.write(self.len(), value);
+        self.length += 1;
+    }
+
+    pub fn pop(&mut self) -> bool {
+        let value = self.read(self.len() - 1);
+        self.write(self.len() - 1, false);
+        if self.len() / 8 < self.vec.len() {
+            self.vec.pop();
+        }
+        value
     }
 }
--- tests/test.rs
+++ tests/test.rs
@@ -12,9 +12,27 @@ mod tests {
     }
 
     #[test]
-    fn test_read() {
+    fn test_push() {
         let mut vector = BitVec::new();
-        let value = vector.read(5);
-        assert_eq!(value, false);
+        vector.push(true);
+        assert_eq!(vector.vec[0], 1);
+    }
+
+    #[test]
+    fn test_push_many_and_overflow() {
+        let mut vector = BitVec::new();
+        for _ in 0..9 {
+            vector.push(true);
+        }
+        assert_eq!(vector.len(), 9);
+        assert_eq!(vector.vec[0], 0b11111111);
+        assert_eq!(vector.vec[1], 0b00000001);
+    }
+
+    #[test]
+    fn test_pop() {
+        let mut vector = BitVec::new();
+        vector.write(5, true);
+        vec
     }
 }
